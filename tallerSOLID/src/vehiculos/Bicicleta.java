6be6357c6b9c353db2b1fcc3657e8fc3239e830a
Jose Escobar <joanesco@espol.edu.ec>
2018-06-08 19:22:45 -0500

    OCPviolation Solucionada

--- tallerSOLID/src/vehiculos/Bicicleta.java
+++ tallerSOLID/src/vehiculos/Bicicleta.java
@@ -5,30 +5,40 @@ import DIPviolation.LlantaParaBicicleta;
 import LSPviolation.Campana;
 
 public class Bicicleta implements Vehiculo{
-	//Esta clase va relacionada con varias violaciones de principios
-	
-	private LlantaParaBicicleta llanta1;
-	private LlantaParaBicicleta llanta2;
-	private AsientoParaBicicleta asiento;
-	private Campana campanilla;
-	
-	public Bicicleta() {
-		llanta1 = new LlantaParaBicicleta();
-		llanta2 = new LlantaParaBicicleta();
-		asiento = new AsientoParaBicicleta();
-		campanilla = new Campana();
-	}
-	
-	public void girarLlantaDelanteraAIzquierda() {
-		//Codigo para girar llanta delantera a la izquierda
-	}
-	
-	public void girarLlantaDelanteraADerecha() {
-		//Codigo para girar llanta delantera a la izquierda
-	}
+    //Esta clase va relacionada con varias violaciones de principios
+    
+    private LlantaParaBicicleta llanta1;
+    private LlantaParaBicicleta llanta2;
+    private AsientoParaBicicleta asiento;
+    private Campana campanilla;
 
-	@Override
-	public void escuchar() {
-		campanilla.tintinear();
-	}
+    public Bicicleta() {
+            llanta1 = new LlantaParaBicicleta();
+            llanta2 = new LlantaParaBicicleta();
+            asiento = new AsientoParaBicicleta();
+            campanilla = new Campana();
+    }
+
+    public void girarLlantaDelanteraAIzquierda() {
+            //Codigo para girar llanta delantera a la izquierda
+    }
+
+    public void girarLlantaDelanteraADerecha() {
+            //Codigo para girar llanta delantera a la izquierda
+    }
+
+    @Override
+    public void escuchar() {
+            campanilla.tintinear();
+    }
+
+    @Override
+    public void girarDerecha() {
+	girarLlantaDelanteraAIzquierda();
+    }
+
+    @Override
+    public void girarIzquierda() {
+        girarLlantaDelanteraADerecha();
+    }
 }
--- tallerSOLID/src/vehiculos/Carro.java
+++ tallerSOLID/src/vehiculos/Carro.java
@@ -5,47 +5,58 @@ import DIPviolation.LlantaParaCarro;
 import LSPviolation.Radio;
 
 public class Carro implements Vehiculo{
-	//Esta clase va relacionada con varias violaciones de principios
-	
-	private LlantaParaCarro llanta1;
-	private LlantaParaCarro llanta2;
-	private LlantaParaCarro llanta3;
-	private LlantaParaCarro llanta4;
-	private AsientoParaCarro asiento1;
-	private AsientoParaCarro asiento2;
-	private Radio radio;
-	
-	public Carro() {
-		llanta1 = new LlantaParaCarro();
-		llanta2 = new LlantaParaCarro();
-		llanta3 = new LlantaParaCarro();
-		llanta4 = new LlantaParaCarro();
-		asiento1 = new AsientoParaCarro();
-		asiento2 = new AsientoParaCarro();
-		radio = new Radio();
-	}
-	
-	public void girarLlantaDelanteraIzquierdaAIzquierda() {
-		
-	}
-	
-	public void girarLlantaDelanteraDerechaAIzquierda() {
-		
-	}
-	
-	public void girarLlantaDelanteraIzquierdaADerecha() {
-		
-	}
-	public void girarLlantaDelanteraDerechaADerecha() {
-		
-	}
+    //Esta clase va relacionada con varias violaciones de principios
 
-	@Override
-	public void escuchar() {
-		
-		if(!radio.isEncendida()) {
-			return;
-		}
-		//Codigo para escuchar la radio
-	}
+    private LlantaParaCarro llanta1;
+    private LlantaParaCarro llanta2;
+    private LlantaParaCarro llanta3;
+    private LlantaParaCarro llanta4;
+    private AsientoParaCarro asiento1;
+    private AsientoParaCarro asiento2;
+    private Radio radio;
+
+    public Carro() {
+            llanta1 = new LlantaParaCarro();
+            llanta2 = new LlantaParaCarro();
+            llanta3 = new LlantaParaCarro();
+            llanta4 = new LlantaParaCarro();
+            asiento1 = new AsientoParaCarro();
+            asiento2 = new AsientoParaCarro();
+            radio = new Radio();
+    }
+
+    public void girarLlantaDelanteraIzquierdaAIzquierda() {
+
+    }
+
+    public void girarLlantaDelanteraDerechaAIzquierda() {
+
+    }
+
+    public void girarLlantaDelanteraIzquierdaADerecha() {
+
+    }
+    public void girarLlantaDelanteraDerechaADerecha() {
+
+    }
+
+    @Override
+    public void escuchar() {
+        if(!radio.isEncendida()) {
+                return;
+        }
+        //Codigo para escuchar la radio
+    }
+
+    @Override
+    public void girarDerecha() {
+        girarLlantaDelanteraIzquierdaADerecha();
+	girarLlantaDelanteraDerechaADerecha();
+    }
+
+    @Override
+    public void girarIzquierda() {
+        girarLlantaDelanteraIzquierdaAIzquierda();
+	girarLlantaDelanteraDerechaAIzquierda();
+    }
 }
